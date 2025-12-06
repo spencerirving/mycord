497db744b1f1734ff7bdb4a3c39925d1769db92e
Spencer Irving <spirving15@gmail.com>
2025-12-06 15:51:14 -0500

    implementation for printing the help message

--- client.c
+++ client.c
@@ -27,6 +27,25 @@ static char* COLOR_GRAY = "\033[90m";
 static char* COLOR_RESET = "\033[0m";
 static settings_t settings = {0};
 
+
+void print_help() { 
+	// this function prints the help function out to stdout when called by the main function
+	fprintf(stdout, "usage: ./client [-h] [--port PORT] [--ip IP] [--domain DOMAIN] [--quiet]\n\n"
+
+		"mycord client\n\n"
+		"options:\n"
+		"  --help                show this help message and exit\n"
+		"  --port PORT           port to connect to (default: 8080)\n"
+		"  --ip IP               IP to connect to (default: \"127.0.0.1\")\n"
+		"  --domain DOMAIN       Domain name to connect to (if domain is specified, IP must not be)\n"
+		"  --quiet               do not perform alerts or mention highlighting\n\n"
+		"examples:\n"
+		"  ./client --help (prints the above message)\n"
+		"  ./client --port 1738 (connects to a mycord server at 127.0.0.1:1738)\n"
+		"  ./client --domain example.com (connects to a mycord server at example.com:8080)\n"
+  );
+}
+
 int process_args(int argc, char *argv[]) {
     return -1;
 }
@@ -76,4 +95,5 @@ int main(int argc, char *argv[]) {
     // wait for the thread / clean up
 
     // cleanup and return
+    print_help();
 }
